e30bdffe50130b46c1e689be3368533da3a92c71
Kashyap Jagwani <kashyap@nishaan.info>
2026-07-01 16:48:04 +0530

    favicon updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,25 +30,6 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <link rel="icon" href="/favicon.ico" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/site.webmanifest" />
       <body className="min-h-full flex flex-col">
         <TabTitleChanger />
         {children}
